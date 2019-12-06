055e634514aa841b5989e217ab509980981401cb
yigitfindikli <yigitfindikli@fd9223c1-c77d-41e1-af93-17165896985c>
2019-12-06 14:47:21 +0000

    Config added.

---
+++ src/app/app.config.component.ts
@@ -0,0 +1,204 @@
+import {Component, OnInit} from '@angular/core';
+import {trigger, state, style, transition, animate} from '@angular/animations';
+import {AppComponent} from './app.component';
+
+@Component({
+    selector: 'app-config',
+    template: `
+        <div class="layout-config" [ngClass]="{'layout-config-active': app.configActive}" (click)="app.onConfigClick($event)">
+            <div class="layout-config-content">
+                <a style="cursor: pointer" id="layout-config-button" class="layout-config-button" (click)="onConfigButtonClick($event)">
+                    <i class="pi pi-cog"></i>
+                </a>
+                <a style="cursor: pointer" class="layout-config-close" (click)="onConfigCloseClick($event)">
+                    <i class="pi pi-times"></i>
+                </a>
+                <p-tabView>
+                    <p-tabPanel header="Menu">
+                        <h1>Menu Modes</h1>
+                        <div class="p-grid">
+                            <div class="p-col p-col-fixed">
+                                <a style="cursor: pointer" class="layout-config-option-image ui-shadow-1" (click)="app.layoutMode = 'static'">
+                                    <img src="assets/layout/images/configurator/menu/babylon-static.png" alt="babylon"/>
+                                    <i class="pi pi-check" *ngIf="app.layoutMode === 'static'"></i>
+                                </a>
+                                <span>Static</span>
+                            </div>
+                            <div class="p-col p-col-fixed">
+                                <a style="cursor: pointer" class="layout-config-option-image ui-shadow-1" (click)="app.layoutMode = 'overlay'">
+                                    <img src="assets/layout/images/configurator/menu/babylon-overlay.png" alt="babylon"/>
+                                    <i class="pi pi-check" *ngIf="app.layoutMode === 'overlay'"></i>
+                                </a>
+                                <span>Overlay</span>
+                            </div>
+                            <div class="p-col p-col-fixed">
+                                <a style="cursor: pointer" class="layout-config-option-image ui-shadow-1" (click)="app.layoutMode = 'horizontal'">
+                                    <img src="assets/layout/images/configurator/menu/babylon-horizontal.png" alt="babylon"/>
+                                    <i class="pi pi-check" *ngIf="app.layoutMode === 'horizontal'"></i>
+                                </a>
+                                <span>Horizontal</span>
+                            </div>
+                            <div class="p-col p-col-fixed">
+                                <a style="cursor: pointer" class="layout-config-option-image ui-shadow-1"
+                                   (click)="app.layoutMode = 'slim'">
+                                    <img src="assets/layout/images/configurator/menu/babylon-slim.png" alt="babylon"/>
+                                    <i class="pi pi-check" *ngIf="app.layoutMode === 'slim'"></i>
+                                </a>
+                                <span>Slim</span>
+                            </div>
+                        </div>
+                        <h1>Menu Type</h1>
+                        <div class="p-grid">
+                            <div class="p-col p-col-fixed">
+                                <a style="cursor: pointer" class="layout-config-option-image ui-shadow-1" (click)="this.app.grouped = true">
+                                    <img src="assets/layout/images/configurator/menu/babylon-grouped.png" alt="babylon"/>
+                                    <i class="pi pi-check" *ngIf="app.grouped"></i>
+                                </a>
+                                <span>Grouped</span>
+                            </div>
+                            <div class="p-col p-col-fixed">
+                                <a style="cursor: pointer" class="layout-config-option-image ui-shadow-1" (click)="this.app.grouped = false">
+                                    <img src="assets/layout/images/configurator/menu/babylon-ungrouped.png" alt="babylon"/>
+                                    <i class="pi pi-check" *ngIf="!app.grouped"></i>
+                                </a>
+                                <span>Ungrouped</span>
+                            </div>
+                        </div>
+                        <h1>Menu Colors</h1>
+                        <div class="p-grid">
+                            <div class="p-col p-col-fixed">
+                                <a style="cursor: pointer" class="layout-config-option-image ui-shadow-1" (click)="this.app.darkMenu = true">
+                                    <img src="assets/layout/images/configurator/menu/babylon-static.png" alt="babylon"/>
+                                    <i class="pi pi-check" *ngIf="app.darkMenu"></i>
+                                </a>
+                                <span>Dark</span>
+                            </div>
+                            <div class="p-col p-col-fixed">
+                                <a style="cursor: pointer" class="layout-config-option-image ui-shadow-1" (click)="this.app.darkMenu = false">
+                                    <img src="assets/layout/images/configurator/menu/babylon-light.png" alt="babylon"/>
+                                    <i class="pi pi-check" *ngIf="!app.darkMenu"></i>
+                                </a>
+                                <span>Light</span>
+                            </div>
+                        </div>
+                    </p-tabPanel>
+                    <p-tabPanel header="User Profile">
+                        <div class="p-grid">
+                            <div class="p-col p-col-fixed">
+                                <a style="cursor: pointer" class="layout-config-option-image ui-shadow-1" (click)="this.app.profileMode = 'inline'">
+                                    <img src="assets/layout/images/configurator/menu/babylon-inline.png" alt="babylon"/>
+                                    <i class="pi pi-check" *ngIf="app.profileMode === 'inline'"></i>
+                                </a>
+                                <span>Inline</span>
+                            </div>
+                            <div class="p-col p-col-fixed">
+                                <a style="cursor: pointer" class="layout-config-option-image ui-shadow-1" (click)="this.app.profileMode = 'popup'">
+                                    <img src="assets/layout/images/configurator/menu/babylon-popup.png" alt="babylon"/>
+                                    <i class="pi pi-check" *ngIf="app.profileMode === 'popup'"></i>
+                                </a>
+                                <span>Popup</span>
+                            </div>
+                        </div>
+                    </p-tabPanel>
+                    <p-tabPanel header="Themes">
+                        <div class="p-grid">
+                            <div class="p-col p-xl-2" *ngFor="let componentTheme of componentThemes">
+                                <a style="cursor: pointer" class="layout-config-option-image layout-config-option ui-shadow-1" (click)="app.changeTheme(componentTheme.name,componentTheme.file)">
+                                    <img src="assets/layout/images/configurator/themes/{{componentTheme.image}}" alt="babylon"/>
+                                    <i class="pi pi-check" *ngIf="app.themeColor === componentTheme.name + '-' + componentTheme.file"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </p-tabPanel>
+                </p-tabView>
+            </div>
+        </div>
+    `,
+    animations: [
+        trigger('children', [
+            state('hiddenAnimated', style({
+                // opacity: 0,
+                // transform: ' translateX(-50%) translateY(-50%)'
+            })),
+            state('visibleAnimated', style({
+                // opacity: 1,
+                transform: 'translateX(-50%) translateY(-50%) scale(1)',
+            })),
+            transition('visibleAnimated => hiddenAnimated', animate('150ms cubic-bezier(0, 0, 0.2, 1)')),
+            transition('hiddenAnimated => visibleAnimated', animate('150ms cubic-bezier(0, 0, 0.2, 1)'))
+        ])
+    ]
+})
+export class AppConfigComponent implements OnInit {
+
+    componentThemes: any;
+
+    constructor(public app: AppComponent) {}
+
+    ngOnInit() {
+        this.componentThemes = [
+            {name: 'amber', file: 'accent', image: 'amber-accent.svg'},
+            {name: 'amber', file: 'light', image: 'amber-light.svg'},
+            {name: 'amber', file: 'dark', image: 'amber-dark.svg'},
+            {name: 'blue', file: 'accent', image: 'blue-accent.svg'},
+            {name: 'blue', file: 'light', image: 'blue-light.svg'},
+            {name: 'blue', file: 'dark', image: 'blue-dark.svg'},
+            {name: 'bluegrey', file: 'accent', image: 'bluegrey-accent.svg'},
+            {name: 'bluegrey', file: 'light', image: 'bluegrey-light.svg'},
+            {name: 'bluegrey', file: 'dark', image: 'bluegrey-dark.svg'},
+            {name: 'brown', file: 'accent', image: 'brown-accent.svg'},
+            {name: 'brown', file: 'light', image: 'brown-light.svg'},
+            {name: 'brown', file: 'dark', image: 'brown-dark.svg'},
+            {name: 'cyan', file: 'accent', image: 'cyan-accent.svg'},
+            {name: 'cyan', file: 'light', image: 'cyan-light.svg'},
+            {name: 'cyan', file: 'dark', image: 'cyan-dark.svg'},
+            {name: 'deeporange', file: 'accent', image: 'deeporange-accent.svg'},
+            {name: 'deeporange', file: 'light', image: 'deeporange-light.svg'},
+            {name: 'deeporange', file: 'dark', image: 'deeporange-dark.svg'},
+            {name: 'deeppurple', file: 'accent', image: 'deeppurple-accent.svg'},
+            {name: 'deeppurple', file: 'light', image: 'deeppurple-light.svg'},
+            {name: 'deeppurple', file: 'dark', image: 'deeppurple-dark.svg'},
+            {name: 'green', file: 'accent', image: 'green-accent.svg'},
+            {name: 'green', file: 'light', image: 'green-light.svg'},
+            {name: 'green', file: 'dark', image: 'green-dark.svg'},
+            {name: 'indigo', file: 'accent', image: 'indigo-accent.svg'},
+            {name: 'indigo', file: 'light', image: 'indigo-light.svg'},
+            {name: 'indigo', file: 'dark', image: 'indigo-dark.svg'},
+            {name: 'lightblue', file: 'accent', image: 'lightblue-accent.svg'},
+            {name: 'lightblue', file: 'light', image: 'lightblue-light.svg'},
+            {name: 'lightblue', file: 'dark', image: 'lightblue-dark.svg'},
+            {name: 'lightgreen', file: 'accent', image: 'lightgreen-accent.svg'},
+            {name: 'lightgreen', file: 'light', image: 'lightgreen-light.svg'},
+            {name: 'lightgreen', file: 'dark', image: 'lightgreen-dark.svg'},
+            {name: 'lime', file: 'accent', image: 'lime-accent.svg'},
+            {name: 'lime', file: 'light', image: 'lime-light.svg'},
+            {name: 'lime', file: 'dark', image: 'lime-dark.svg'},
+            {name: 'orange', file: 'accent', image: 'orange-accent.svg'},
+            {name: 'orange', file: 'light', image: 'orange-light.svg'},
+            {name: 'orange', file: 'dark', image: 'orange-dark.svg'},
+            {name: 'pink', file: 'accent', image: 'pink-accent.svg'},
+            {name: 'pink', file: 'light', image: 'pink-light.svg'},
+            {name: 'pink', file: 'dark', image: 'pink-dark.svg'},
+            {name: 'purple', file: 'accent', image: 'purple-accent.svg'},
+            {name: 'purple', file: 'light', image: 'purple-light.svg'},
+            {name: 'purple', file: 'dark', image: 'purple-dark.svg'},
+            {name: 'teal', file: 'accent', image: 'teal-accent.svg'},
+            {name: 'teal', file: 'light', image: 'teal-light.svg'},
+            {name: 'teal', file: 'dark', image: 'teal-dark.svg'},
+            {name: 'yellow', file: 'accent', image: 'yellow-accent.svg'},
+            {name: 'yellow', file: 'light', image: 'yellow-light.svg'},
+            {name: 'yellow', file: 'dark', image: 'yellow-dark.svg'}
+        ];
+    }
+
+
+    onConfigButtonClick(event) {
+        this.app.configActive = !this.app.configActive;
+        event.preventDefault();
+    }
+
+    onConfigCloseClick(event) {
+        this.app.configActive = false;
+        event.preventDefault();
+    }
+}
