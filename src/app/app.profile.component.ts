78029eb40c090ee85e2721d612615c05cbbdd57a
merve7 <merve7@fd9223c1-c77d-41e1-af93-17165896985c>
2019-12-11 14:49:07 +0000

    slim profile animation fix

--- src/app/app.profile.component.ts
+++ src/app/app.profile.component.ts
@@ -1,25 +1,36 @@
 import {Component} from '@angular/core';
 import {trigger, state, transition, style, animate} from '@angular/animations';
+import {AppComponent} from './app.component';
 
 @Component({
     selector: 'app-inline-profile',
     templateUrl: './app.profile.component.html',
     animations: [
         trigger('menu', [
-            state('hidden', style({
+            state('hiddenAnimated', style({
                 height: '0px'
             })),
-            state('visible', style({
+            state('visibleAnimated', style({
                 height: '*'
             })),
-            transition('visible => hidden', animate('400ms cubic-bezier(0.86, 0, 0.07, 1)')),
-            transition('hidden => visible', animate('400ms cubic-bezier(0.86, 0, 0.07, 1)'))
+            state('visible', style({
+                height: '*',
+                'z-index': 100
+            })),
+            state('hidden', style({
+                height: '0px',
+                'z-index': '*'
+            })),
+            transition('visibleAnimated => hiddenAnimated', animate('400ms cubic-bezier(0.86, 0, 0.07, 1)')),
+            transition('hiddenAnimated => visibleAnimated', animate('400ms cubic-bezier(0.86, 0, 0.07, 1)'))
         ])
     ]
 })
 export class AppProfileComponent {
 
     active: boolean;
+
+    constructor(public app: AppComponent) { }
 
     onClick(event) {
         this.active = !this.active;
