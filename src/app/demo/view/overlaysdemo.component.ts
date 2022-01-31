c259a88bfe3d372f133221e437d246c309056d43
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2022-01-31 09:25:35 +0300

    update overlays, crud & buttondemo

--- src/app/demo/view/overlaysdemo.component.ts
+++ src/app/demo/view/overlaysdemo.component.ts
@@ -79,4 +79,8 @@ export class OverlaysDemoComponent implements OnInit {
             }
         });
     }
+
+    formatCurrency(value) {
+        return value.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
+    }
 }
--- src/app/pages/app.crud.component.ts
+++ src/app/pages/app.crud.component.ts
@@ -7,23 +7,7 @@ import {BreadcrumbService} from '../app.breadcrumb.service';
 
 @Component({
     templateUrl: './app.crud.component.html',
-    styles: [`
-        :host ::ng-deep .p-dialog .product-image {
-            width: 150px;
-            margin: 0 auto 2rem auto;
-            display: block;
-        }
-
-        @media screen and (max-width: 960px) {
-            :host ::ng-deep .p-datatable.p-datatable-customers .p-datatable-tbody > tr > td:last-child {
-                text-align: center;
-            }
-
-            :host ::ng-deep .p-datatable.p-datatable-customers .p-datatable-tbody > tr > td:nth-child(6) {
-                display: flex;
-            }
-        }
-    `],
+    styleUrls: ['../../assets/demo/badges.scss'],
     providers: [MessageService, ConfirmationService]
 })
 export class AppCrudComponent implements OnInit {
